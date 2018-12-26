6a1e6a831256a311443ffefe508f165802ab9fd5
Steve Torrens <stevetorrens70@gmail.com>
2018-12-27 10:48:53 +1300

    working on nasa exercise

--- nasa-api/nasa-images.js
+++ nasa-api/nasa-images.js
@@ -3,11 +3,21 @@
 var apiKey = '&api_key=0pBInYTfAommoMM3AgVsn3o1UKYAAASxnCLo2avX'
 document.getElementById('load-indicator').style.display = "none"
 
-function getNASAimages(event) {
+function getNASAimages(event, searchText = 'Apollo') {
   event.preventDefault()
-  searchText = document.getElementById('form-input').value
-  document.getElementsByClassName('image-search')[0].style.display = "none"
-  document.getElementById('load-indicator').style.display = "block"
+  searchText = document.getElementById('form-input').value || searchText
+  fromStorage = localStorage.getItem(searchText)
+  console.log(fromStorage)
+  if (!fromStorage) {
+    document.getElementById('load-indicator').style.display = "block"
+    fetchResults(searchText)
+  } else {
+    console.log(fromStorage)
+    showImages(fromStorage.json())
+  }
+}
+
+function fetchResults(searchText) {
   fetch(`https://images-api.nasa.gov/search?q=${searchText}&media_type=image`)
     .then(function(r) {
       switch (r.status) {
@@ -23,82 +33,47 @@ function getNASAimages(event) {
     })
     .then(function(j) {
       showImages(j)
+      //Local storage key - value pairs must both be strings
+      localStorage.setItem(searchText,j.stringify())
     })
     .catch(function(j) {
       console.log("oh no")
     })
 }
 
+function showError(error) {
+  errorPara = document.createElement("p")
+  errorMessage = document.createTextNode(`Status: ${error}`)
+  document.getElementById("nasa-images").appendChild()
+
+}
+
 function showImages(results) {
   document.getElementsByClassName('image-search')[0].style.display = "block"
   document.getElementById('load-indicator').style.display = "none"
   results.collection.items.map((item) => {
+    let { links, data, href } = item
     let imageDiv = document.createElement("div")
     let imageLink = document.createElement("img")
-    imageLink.setAttribute("src", item.links[0].href)
+    imageLink.setAttribute("src", links[0].href)
     imageLink.setAttribute("onmouseover", "showCaption(event)")
+    imageLink.setAttribute("onmouseleave", "hideCaption(event)")
     let imageCaption = document.createElement("p")
-    let caption = document.createTextNode(item.data[0].description)
+    imageCaption.style.display = "none"
+    let caption = document.createTextNode(data[0].description || '')
     document.getElementById("nasa-images").appendChild(imageDiv)
     imageDiv.appendChild(imageLink)
     imageDiv.appendChild(imageCaption)
-    imageCaption.style.display = "none"
     imageCaption.appendChild(caption)
   })
-
 
 }
 
 function showCaption(event) {
-  console.log(event.target)
-  event.target.style.display = "block"
+  console.log(event.target.parentNode.getElementsByTagName('p'))
+  event.target.parentNode.getElementsByTagName('p')[0].style.display = "block"
 }
-// https://api.nasa.gov/planetary/apod?
-// fetchGitHubData('steve-dev90')
 
-// function getGitHubData(event) {
-//   event.preventDefault()
-//   parent = document.getElementById('follower_info')
-//   children = document.getElementsByTagName('li')
-//   // Array.from converts array like object to an array
-//   Array.from(children).map((child) => {parent.removeChild(child)})
-//   name = document.getElementById('form-input').value
-//   fetchGitHubData(name)
-//   document.getElementById("form").reset()
-// }
-
-// function fetchGitHubData(name) {
-//   document.getElementsByClassName('container')[0].style.display = "none"
-
-//   fetch(`https://api.github.com/users/${name}`)
-//     .then(function(r) {
-//       return r.json();
-//     })
-//     .then(function(j) {
-//       console.log(j);
-//       document.getElementById('avatar').innerText = `Avatar: ${j.avatar_url}`;
-//       document.getElementById('username').innerText = `Username: ${j.login}`;
-//       document.getElementById('real-name').innerText = `Real-name: ${j.name}`;
-//       document.getElementById('location').innerText = `Location:  ${j.login}`;
-//       document.getElementById('bio').innerText = `Bio:  ${j.bio}`;
-//       document.getElementById('followers').innerText = `Followers: ${j.followers}`;
-//     })
-//     .then(function() {
-//       document.getElementById('load-indicator').style.display = "none"
-//       document.getElementsByClassName('container')[0].style.display = "block"
-//     })
-
-//   fetch(`https://api.github.com/users/${name}/followers`)
-//     .then(function(r) {
-//       return r.json();
-//     })
-//     .then(function(j) {
-//       console.log(j);
-//       j.map(function(follower) {
-//         var listItem = document.createElement('li')
-//         var listText = document.createTextNode(`login: ${follower.login} avatar: ${follower.avatar_url}`)
-//         document.getElementById('follower_info').appendChild(listItem)
-//         listItem.appendChild(listText)
-//       })
-//     })
-// }
+function hideCaption(event) {
+  event.target.parentNode.getElementsByTagName('p')[0].style.display = "none"
+}
